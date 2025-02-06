699db93d4e796d07d69689110022a7c8ae9bcabc
BUXLANKA <matwey.ishekov@gmail.com>
2025-02-07 02:17:03 +0300

    ShowHide реализация костылями

--- lua/recipies/gmod_subway_81-717_lvz/bumper/717spb_bumper.lua
+++ lua/recipies/gmod_subway_81-717_lvz/bumper/717spb_bumper.lua
@@ -10,13 +10,8 @@ function RECIPE:InjectSpawner(entclass)
         [3] = "List",
         [4] = {"type-1", "type-2", "empty"}
     })
-    -- MEL.AddSpawnerField(entclass, {
-    --     [1] = "vlzMaskType",
-    --     [2] = "Spawner.717.newmasktype",
-    --     [3] = "List",
-    --     [4] = {"141", "131", "222"}
-    -- })
 end
+
 function RECIPE:Inject(ent, entclass)
     MEL.NewClientProp(ent, "lvzBumper", {
         model = root_model .. "bamp_typ1.mdl",
@@ -26,22 +21,13 @@ function RECIPE:Inject(ent, entclass)
         modelcallback = function(wagon, cent)
             local models = {
                 root_model .. "bamp_typ1.mdl",
-                root_model .. "bamp_typ2.mdl",
+                root_model .. "bamp_typ2.mdl"
             }
         return models[wagon:GetNW2Int("vlzBumpType")] end
     }, "vlzBumpType")
 
-    -- MEL.NewClientProp(ent, "vlzNewMask", {
-    --     model = root_mask .. "141.mdl",
-    --     pos = Vector(50,0,0),
-    --     ang = Angle(0,-90,0),
-
-    --     modelcallback = function(wagon, cent)
-    --         local models = {
-    --             root_mask .. "540_141.mdl",
-    --             root_mask .. "540_131.mdl",
-    --             root_mask .. "540_222.mdl",
-    --         }
-    --     return models[wagon:GetNW2Int("vlzMaskType")] end
-    -- }, "vlzMaskType")
+    MEL.UpdateCallback(ent, "lvzBumper", function(ent, cent)
+        local bt = ent:GetNW2Int("vlzBumpType")
+        if bt == 3 then cent:SetNoDraw(true) end
+    end)
 end
