888f62858c5526d6b1dcdabc2f9105d0b2559ae0
BUXLANKA <matwey.ishekov@gmail.com>
2025-02-07 22:47:40 +0300

    лангуаге ве два

--- lua/metrostroi_data/languages/ru_717bumpers.lua
+++ lua/metrostroi_data/languages/ru_717bumpers.lua
@@ -1,24 +1,31 @@
 return [[
 [ru]
 Entities.gmod_subway_81-717_lvz_custom.Spawner.vlzBumpType.Name = Тип бампера
-Entities.gmod_subway_81-717_lvz_custom.Spawner.vlzBumpType.type-1 = Тип 1
-Entities.gmod_subway_81-717_lvz_custom.Spawner.vlzBumpType.type-2 = Тип 2
-Entities.gmod_subway_81-717_lvz_custom.Spawner.vlzBumpType.type-3 = Без бампера
+Entities.gmod_subway_81-717_lvz_custom.Spawner.vlzBumpType.1 = Тип 1
+Entities.gmod_subway_81-717_lvz_custom.Spawner.vlzBumpType.2 = Тип 2
+Entities.gmod_subway_81-717_lvz_custom.Spawner.vlzBumpType.3 = Без бампера
 
 Entities.gmod_subway_81-717_lvz_custom.Spawner.PassTexture.Name = @[Common.Spawner.PassTexture]
 Entities.gmod_subway_81-717_lvz_custom.Spawner.CabTexture.Name = @[Common.Spawner.CabTexture]
 
-Entities.gmod_subway_81-717_lvz.Spawner.Texture.Name        = @[Common.Spawner.Texture]
-Entities.gmod_subway_81-717_lvz.Spawner.PassTexture.Name    = @[Common.Spawner.PassTexture]
-Entities.gmod_subway_81-717_lvz.Spawner.CabTexture.Name     = @[Common.Spawner.CabTexture]
-Entities.gmod_subway_81-717_lvz.Spawner.Scheme.Name         = @[Common.Spawner.Scheme]
-Entities.gmod_subway_81-717_lvz.Spawner.Type.Name           = @[Spawner.Common.EType]
-Entities.gmod_subway_81-717_lvz.Spawner.Type.1              = @[Spawner.717.Line2]
-Entities.gmod_subway_81-717_lvz.Spawner.Type.2              = @[Spawner.717.Line4]
-Entities.gmod_subway_81-717_lvz.Spawner.Type.3              = @[Spawner.717.Line5]
-Entities.gmod_subway_81-717_lvz.Spawner.SpawnMode.Name      = @[Common.Spawner.SpawnMode]
-Entities.gmod_subway_81-717_lvz.Spawner.SpawnMode.1         = @[Common.Spawner.SpawnMode.Full]
-Entities.gmod_subway_81-717_lvz.Spawner.SpawnMode.2         = @[Common.Spawner.SpawnMode.Deadlock]
-Entities.gmod_subway_81-717_lvz.Spawner.SpawnMode.3         = @[Common.Spawner.SpawnMode.NightDeadlock]
-Entities.gmod_subway_81-717_lvz.Spawner.SpawnMode.4         = @[Common.Spawner.SpawnMode.Depot]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.Name = @[Spawner.Common.EType]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.1 = @[Spawner.717.Line2]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.2 = @[Spawner.717.Line4]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.3 = @[Spawner.717.Line5]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.Name = @[Common.Spawner.SpawnMode]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.1 = @[Common.Spawner.SpawnMode.Full]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.2 = @[Common.Spawner.SpawnMode.Deadlock]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.3 = @[Common.Spawner.SpawnMode.NightDeadlock]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.4 = @[Common.Spawner.SpawnMode.Depot]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SalonLampType.Name = @[Spawner.717.LampType]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SalonLampType.1 = @[Common.Spawner.Random]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SalonLampType.2 = @[Spawner.717.Lamp1]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SalonLampType.3 = @[Spawner.717.Lamp2]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.WagonType.Name = @[Spawner.717.BodyType]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SeatType.Name = lol (?)
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.540maskType.Name = Тип маски
 ]]
